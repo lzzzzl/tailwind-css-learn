38d77c90fa8a2f120380f242cc0bb14738f02750
lzzzzl <lzzzzl005@gmail.com>
2023-07-03 23:14:46 +0800

    #3 Navigation

--- vue-tailwindcss-weather-app/src/App.vue
+++ vue-tailwindcss-weather-app/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigationVue />
     <RouterView />
   </div>
 </template>
 
-<script>
+<script setup>
 import { RouterView } from 'vue-router';
-export default {
+import SiteNavigationVue from './components/SiteNavigation.vue';
 
-}
+
 </script>
 
 <style>
