87f470e63c65bc321e4a3f90f09e0569f5aac238
StepBackLin <597023590@qq.com>
2023-07-05 08:08:34 +0800

    #6 - Handling API Errors

--- vue-tailwindcss-weather-app/src/views/HomeView.vue
+++ vue-tailwindcss-weather-app/src/views/HomeView.vue
@@ -4,9 +4,17 @@
       <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="search for a city or state" class="py-2 px-1 w-full bg-transparent border-b
       focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
       <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="searchResults">
-        <li v-for="searchResult in searchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p v-if="!searchError && searchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li v-for="searchResult in searchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -18,19 +26,22 @@ import axios from "axios";
 
 const searchQuery = ref("");
 const searchResults = ref(null);
+const searchError = ref(null);
 const queryTimeout = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value != "") {
-      const result = await axios.get(`http://localhost:3000/weather?q=${searchQuery.value}`)
-      let firstDataFeatures = null;
-      if (Array.isArray(result.data) && result.data.length > 0) {
-        searchResults.value = result.data;
-        console.log(result.data)
-        return
+      try {
+        const result = await axios.get(`http://localhost:3000/weather?q=${searchQuery.value}`)
+        if (Array.isArray(result.data) && result.data.length > 0) {
+          searchResults.value = result.data;
+        }
+      } catch {
+        searchError.value = true;
       }
+      return
     }
     searchResults.value = null;
   }, 300)
