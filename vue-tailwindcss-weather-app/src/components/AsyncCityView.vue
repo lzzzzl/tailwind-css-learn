21ce25d5e6d17893ee933b44dc6f09f0d25d2f07
StepBackLin <597023590@qq.com>
2023-07-07 08:02:21 +0800

    #9 - City View Layout

--- vue-tailwindcss-weather-app/src/components/AsyncCityView.vue
+++ vue-tailwindcss-weather-app/src/components/AsyncCityView.vue
@@ -30,14 +30,63 @@
             <p class="text-8xl mb-8">
                 {{ Math.round(weatherData.temperature) }} &deg;
             </p>
-            <div class="text-center">
-                <p>
-                    Feels like
-                    {{ Math.round(weatherData.feels_like) }} &deg;
-                </p>
-                <p class="capitalize">
-                    {{ weatherData.description }}
-                </p>
+            <p>
+                Feels like
+                {{ Math.round(weatherData.feels_like) }} &deg;
+            </p>
+            <p class="capitalize">
+                {{ weatherData.description }}
+            </p>
+            <i class="fa-solid fa-cloud text-6xl"></i>
+        </div>
+
+        <hr class="border-white border-opacity-10 border w-full" />
+
+        <!-- Hourly Weather-->
+        <div class="max-w-screen-md w-full py-12">
+            <div class="mx-8 text-white">
+                <h2 class="mb-4">Hourly Weather</h2>
+                <div class="flex gap-10 overflow-x-scroll">
+                    <div v-for="hourData in weatherData.hourly" :key="hourData.dt" class="flex flex-col gap-4 items-center">
+                        <p class="whitespace-nowrap text-md">
+                            {{
+                                new Date(hourData.currentTime).toLocaleTimeString(
+                                    "en-us", 
+                                    {
+                                        hour: "numeric",
+                                    }
+                                )
+                            }}
+                        </p>
+                        <i class="fa-solid fa-cloud-bolt text-2xl"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr class="border-white border-opacity-10 border w-full" />
+
+        <!-- Weekly Weather -->
+        <div class="max-w-screen-md w-full py-12">
+            <div class="mx-8 text-white">
+                <h2 class="mb-4">7 Day Forecast</h2>
+                <div v-for="day in weatherData.hourly" :key="day.dt" class="flex items-center">
+                    <p class="flex-1">
+                        {{
+                            new Date(day.dt * 24 * 60 * 60 * 1000).toLocaleDateString(
+                                "en-us",
+                                {
+                                    weekday: "long"
+                                }
+                            )
+                        }}
+                    </p>
+                    <i class="fa-solid fa-cloud-rain text-2xl"></i>
+                    <div class="flex gap-2 flex-1 justify-end">
+                        <p>H: {{ Math.round(day.temp.max) }}</p>
+                        <p>L: {{ Math.round(day.temp.min) }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -55,7 +104,89 @@ const getWeatherData = async () => {
         weatherData.data.temperature = 81;
         weatherData.data.feels_like = 85;
         weatherData.data.description = "Scattered Clouds";
-
+        weatherData.data.hourly = [
+            {
+                "dt": "1",
+                "currentTime": new Date().getTime() - 3600000 * 9,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "2",
+                "currentTime": new Date().getTime() - 3600000 * 8,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "3",
+                "currentTime": new Date().getTime() - 3600000 * 7,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "4",
+                "currentTime": new Date().getTime() - 3600000 * 6,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "5",
+                "currentTime": new Date().getTime() - 3600000 * 5,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "6",
+                "currentTime": new Date().getTime() - 3600000 * 4,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "7",
+                "currentTime": new Date().getTime() - 3600000 * 3,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "8",
+                "currentTime": new Date().getTime() - 3600000 * 2,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "9",
+                "currentTime": new Date().getTime() - 3600000 * 1,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            },
+            {
+                "dt": "10",
+                "currentTime": new Date().getTime() - 3600000 * 0,
+                "temp": {
+                    "max": 45,
+                    "min": 25
+                }
+            }
+        ]
+        console.log(weatherData.data.hourly)
 
         return weatherData.data
     } catch (err) {
